4d49bd42171bf3c03b51a9665a20ccec63cb1660
NAVIN KRISHNA <tnkrishnank@gmail.com>
2024-09-19 22:48:20 +0530

    Implemented Logging

--- AUTH2_MICROSERVICE/app/__init__.py
+++ AUTH2_MICROSERVICE/app/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
+import logging
 
 api = Api()
 db = SQLAlchemy()
@@ -26,5 +27,30 @@ def create_app():
         api.add_namespace(user_controller.api, path='/user')
 
         db.create_all()
+
+    access_logger = logging.getLogger('access')
+    access_handler = logging.FileHandler('access.log')
+    access_formatter = logging.Formatter('%(clientip)s - - [%(asctime)s] "%(method)s %(path)s %(protocol)s" %(status)s %(size)s')
+    access_handler.setFormatter(access_formatter)
+    access_logger.addHandler(access_handler)
+    access_logger.setLevel(logging.INFO)
+
+    @app.before_request
+    def log_request_info():
+        request.log_info = {
+            'clientip': request.remote_addr,
+            'method': request.method,
+            'path': request.path,
+            'protocol': request.environ.get('SERVER_PROTOCOL'),
+            'status': '-',
+            'size': '-'
+        }
+
+    @app.after_request
+    def log_response_info(response):
+        request.log_info['status'] = response.status_code
+        request.log_info['size'] = response.content_length or 0
+        access_logger.info('', extra=request.log_info)
+        return response
 
     return app
